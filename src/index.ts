4e98acad8d84ce08114282ee5cb571e8a1762589
Jean Dresbach <jeanr.dresbach@gmail.com>
2024-03-03 21:13:35 -0300

    tweet crud and routes added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 
 import userRoutes from "./routes/user.routes"
 import authRoutes from "./routes/auth.routes"
+import tweetRoutes from "./routes/tweet.routes"
 
 const app = express()
 app.use(express.json())
@@ -10,6 +11,7 @@ app.use(cors())
 
 app.use(userRoutes)
 app.use(authRoutes)
+app.use(tweetRoutes)
 
 app.listen(3333, () => {
   console.log("🚀 Server ready at: http://localhost:3333 🚀")

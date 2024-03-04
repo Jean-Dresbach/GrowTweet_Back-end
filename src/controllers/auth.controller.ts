a8a42a29e48fbafb9592f9a6241d803807642549
Jean Dresbach <jeanr.dresbach@gmail.com>
2024-03-03 21:12:48 -0300

    Login updated, logout route added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,18 +7,33 @@ const authService = new AuthService()
 export class AuthController {
   public async login(request: Request, response: Response) {
     try {
-      const { email, password } = request.body
+      const { emailOrUsername, password } = request.body
 
-      if (!email || !password) {
+      if (!emailOrUsername || !password) {
         return response.status(400).json({
           code: response.statusCode,
           message: "Preencha os campos obrigatórios."
         })
       }
 
-      const result = await authService.login(email, password)
+      const result = await authService.login(emailOrUsername, password)
 
       return response.status(200).json(result)
+    } catch (error: any) {
+      return response.status(500).json({
+        code: response.statusCode,
+        message: `Erro ao fazer login: ${error.message}`
+      })
+    }
+  }
+
+  public async logout(request: Request, response: Response) {
+    try {
+      const { userId } = request.params
+
+      const result = await authService.logout(userId)
+
+      return response.status(result.code).json(result)
     } catch (error: any) {
       return response.status(500).json({
         code: response.statusCode,
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,20 @@
 import express from "express"
 
 import { AuthController } from "../controllers/auth.controller"
+import { validateId } from "../middlewares/validateId.middleware"
+import { validateLoginToken } from "../middlewares/auth.middleware"
 
 const router = express.Router()
 
 const authController = new AuthController()
 
 router.post("/login", authController.login)
+
+router.post(
+  "/logout/:userId",
+  validateId,
+  validateLoginToken,
+  authController.logout
+)
 
 export default router
